d3376e181d1ff498161460304292d3c06b13c918
yunxianghe <yunxianghe@usc-guestwireless-upc-newsc12557.usc.edu>
2019-03-24 21:53:05 -0700

    c

--- Java SE/src/enumerated/_106_TrafficLight_Switch.java
+++ Java SE/src/enumerated/_106_TrafficLight_Switch.java
@@ -1,39 +1,39 @@
 package enumerated;
 
-import static utils.Print.*;
+import static utils.Print.print;
 
 enum Signal {
-    GREEN, YELLOW, RED,
+	GREEN, YELLOW, RED,
 }
 
 public class _106_TrafficLight_Switch {
 
-    Signal color = Signal.RED;
+	Signal color = Signal.RED;
 
-    public void change() {
-        switch (color) {
-        // Note that you don't have to say Signal.RED in the case statement:
-        case RED:
-            color = Signal.GREEN;
-            break;
-        case GREEN:
-            color = Signal.YELLOW;
-            break;
-        case YELLOW:
-            color = Signal.RED;
-            break;
-        }
-    }
+	public void change() {
+		switch (color) {
+		// Note that you don't have to say Signal.RED in the case statement:
+		case RED:
+			color = Signal.GREEN;
+			break;
+		case GREEN:
+			color = Signal.YELLOW;
+			break;
+		case YELLOW:
+			color = Signal.RED;
+			break;
+		}
+	}
 
-    public String toString() {
-        return "The traffic light is " + color;
-    }
+	public String toString() {
+		return "The traffic light is " + color;
+	}
 
-    public static void main(String[] args) {
-        _106_TrafficLight_Switch t = new _106_TrafficLight_Switch();
-        for (int i = 0; i < 7; i++) {
-            print(t);
-            t.change();
-        }
-    }
+	public static void main(String[] args) {
+		_106_TrafficLight_Switch t = new _106_TrafficLight_Switch();
+		for (int i = 0; i < 7; i++) {
+			print(t);
+			t.change();
+		}
+	}
 }
